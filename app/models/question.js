43bf024eb9b4e17a7048321729b6fff6e31e3457
Marika <mballely@gmail.com>
2016-04-01 08:07:14 -0700

    add question model

---
+++ app/models/question.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  author: DS.attr(),
+  body: DS.attr(),
+  category: DS.attr(),
+  date: DS.attr('date', { defaultValue() { return new Date(); }}),
+  answers: DS.hasMany('answer', { async: true })
+});
