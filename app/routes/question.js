0a1b740fabf87c688ae9769fc3d90ba07a796634
Marika <mballely@gmail.com>
2016-04-01 09:51:52 -0700

    add deleteQuestion functionality and display individual questions on question-details

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    deleteQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    }
+  }
 });
