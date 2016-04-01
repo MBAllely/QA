2d611308c02964a42abdd17efdb92819699eca79
Marika <mballely@gmail.com>
2016-04-01 08:55:20 -0700

    add functionality to addNewQuestion

---
+++ app/components/new-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true)
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author') ? this.get('author'): "",
+        title: this.get('title') ? this.get('title'): "",
+        body: this.get('body') ? this.get('body'): "",
+        category: this.get('category') ? this.get('category'): "",
+        answers: []
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
