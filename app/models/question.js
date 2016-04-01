589d2dced023bb9faa899350259ff3e84023bd88
Marika <mballely@gmail.com>
2016-04-01 08:07:51 -0700

    add notes attribute to question model

--- app/models/question.js
+++ app/models/question.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   author: DS.attr(),
   body: DS.attr(),
   category: DS.attr(),
+  notes: DS.attr(),
   date: DS.attr('date', { defaultValue() { return new Date(); }}),
   answers: DS.hasMany('answer', { async: true })
 });
