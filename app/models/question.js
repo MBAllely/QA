d07661af0cbce378c824c1539ae66096b2651892
Marika <mballely@gmail.com>
2016-04-01 08:17:17 -0700

    remote notes attribute from question model

--- app/models/question.js
+++ app/models/question.js
@@ -5,7 +5,6 @@ export default DS.Model.extend({
   title: DS.attr(),
   body: DS.attr(),
   category: DS.attr(),
-  notes: DS.attr(),
   date: DS.attr('date', { defaultValue() { return new Date(); }}),
   answers: DS.hasMany('answer', { async: true })
 });
