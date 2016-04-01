77666094950f06206b14fef2b791c68d0ecae781
Marika <mballely@gmail.com>
2016-04-01 14:24:14 -0700

    clean up indentation

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -6,17 +6,17 @@ export default Ember.Component.extend({
     updateAnswerForm() {
       this.set('updateAnswerForm', true);
     },
-    
+
     updateAnswer(answer) {
       var params = {
-        author: this.get('author') ? this.get('author'): "",
-        body: this.get('body') ? this.get('body'): "",
+        author: this.get('author'),
+        body: this.get('body'),
         question: this.get('question')
       };
       this.set('updateAnswerForm', false);
       this.sendAction('updateAnswer', answer, params);
     },
-    
+
     deleteAnswer(answer) {
       if (confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('deleteAnswer', answer);
